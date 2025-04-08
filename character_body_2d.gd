b3a84f8c236309813de228d9cf7558130841a547
M4rk1t1nh0 <205313618+M4rk1t1nh0@users.noreply.github.com>
2025-04-08 09:59:08 +0200

    skibidi

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -8,11 +8,15 @@ func _process(delta: float) -> void:
 	var dx := Input.get_axis("moure_esquerra","moure_dreta")
 	velocity.x = 0
 	velocity.x += dx * velocitat
-	
 	if Input.is_action_just_pressed("salta") and is_on_floor():
 		velocity.y += SALT
 	velocity += gravetat * delta
 	move_and_slide()
-	
+	anima(velocity)
 
+func anima(velocitat: Vector2) -> void:
+	if velocitat.x > 0:
+		$MarioBross.flip_h = false
+	if velocitat.x < 0:
+		$MarioBross.flip_h = true
 		
